73216ae029ce3f9213252e19c2cf7efba0b814e8
Nikolay Dorofeev <d.orich@yandex.com>
2021-09-19 20:58:22 +0700

    Lab4 - math training

--- lib/labs/lab4.dart
+++ lib/labs/lab4.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tpu_mobile_labs/support/math_train.dart';
 import 'dart:math';
+
+Random rand = Random();
+
 
 class Lab4 extends StatefulWidget {
   Lab4({Key? key, required this.title}) : super(key: key);
@@ -11,49 +15,121 @@ class Lab4 extends StatefulWidget {
 }
 
 class _Lab4State extends State<Lab4> {
-  late List<String> _textBoxes = [];
-  late Random rand = Random();
+  late MathSample _sampleNow = MathSample();
+  late List<MathSample> _answersHistory = [];
+  late List<int> _answers = [0, 0, 0];
+  late int _rightCounter = 0;
+  late int _wrongCounter = 0;
 
-  void _addText() {
+  void _setAnswers(){
     setState(() {
-      this._textBoxes.add(rand.nextInt(100).toString());
+      _answers = [];
+      _answers.add(this._sampleNow.rightAnswer);
+      _answers.add(this._sampleNow.rightAnswer + rand.nextInt(21) - 10);
+      _answers.add(this._sampleNow.rightAnswer + rand.nextInt(21) - 10);
+      _answers.sort();
+    });
+  }
+
+  _checkAnswer(int answerIndex){
+    return () => setState(() {
+      if (_sampleNow.checkAnswer(this._answers[answerIndex])){
+        _rightCounter += 1;
+      }
+      else {
+        _wrongCounter += 1;
+      }
+      _answersHistory.insert(0, _sampleNow);
+      if (_answersHistory.length > 10)
+        _answersHistory = _answersHistory.sublist(0, 10);
+      _sampleNow = MathSample();
+      _setAnswers();
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    this._setAnswers();
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Container(
+      body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(30),
+              child: Text(
+                  this._sampleNow.sample,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 OutlinedButton(
-                    onPressed: this._addText,
-                    child: Text('Add Number')
+                    onPressed: this._checkAnswer(0),
+                    child: Text(
+                        this._answers[0].toString(),
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold
+                        )
+                    ),
+
+                ),
+                OutlinedButton(
+                    onPressed: this._checkAnswer(1),
+                    child: Text(
+                        this._answers[1].toString(),
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold
+                        )
+                    )
+                ),
+                OutlinedButton(
+                    onPressed: this._checkAnswer(2),
+                    child: Text(
+                        this._answers[2].toString(),
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold
+                        )
+                    )
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Text("Right: $_rightCounter"),
+                ),
+                Container(
+                  child: Text("Wrong: $_wrongCounter"),
                 )
               ],
             ),
             Expanded(
               child: ListView.separated(
                 padding: const EdgeInsets.all(8),
-                itemCount: this._textBoxes.length,
+                itemCount: this._answersHistory.length,
                 itemBuilder: (BuildContext context, int index){
                   return Container(
                       height: 50,
-                      color: Colors.amber[300],
-                      child: Center(child: Text(this._textBoxes[index]))
+                      color: _answersHistory[index].isCorrectAnswer ? Colors.greenAccent : Colors.redAccent,
+                      child: Center(child: Text("${_answersHistory[index].sample} = ${_answersHistory[index].rightAnswer} | ${_answersHistory[index].answer}"))
                   );
                 },
                 separatorBuilder: (BuildContext context, int index) => const Divider(),
               ),
             )
-
           ],
         ),
       ),

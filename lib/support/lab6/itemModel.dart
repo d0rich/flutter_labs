50858069c74d25c00ab0e54cb32ec652b5d5a3e2
Nikolay Dorofeev <d.orich@yandex.com>
2021-11-07 22:13:33 +0700

    Item model for database

---
+++ lib/support/lab6/itemModel.dart
@@ -0,0 +1,17 @@
+class ItemModel{
+  final int id;
+  final String kind;
+  final String title;
+  final double price;
+  final double weight;
+  final String photo;
+
+  ItemModel({
+      required this.id,
+      required this.kind,
+      required this.title,
+      required this.price,
+      required this.weight,
+      required this.photo
+  });
+}

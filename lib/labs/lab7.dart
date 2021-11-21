b5baa57532cab25d733e823b829ba67e059678fb
Nikolay Dorofeev <d.orich@yandex.com>
2021-11-22 00:26:30 +0700

    Get location

--- lib/labs/lab7.dart
+++ lib/labs/lab7.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:location/location.dart';
 import 'package:tpu_mobile_labs/support/lab7/location_point.dart';
 
 class Lab7 extends StatefulWidget {
@@ -17,6 +18,48 @@ class _Lab7State extends State<Lab7> {
   late TextEditingController _nameController = TextEditingController();
   late List<LocationPoint> _points = [];
 
+  late Location _locationService = new Location();
+  late LocationPoint _locationData = new LocationPoint(lat: 0, long: 0, name: "name");
+
+  // Проверка на разрешение геолокации
+  Future<bool> _checkLocationServiceStatus() async{
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationService.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _locationService.requestService();
+      if (!_serviceEnabled) {
+        return false;
+      }
+    }
+
+    _permissionGranted = await _locationService.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationService.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return false;
+      }
+    }
+
+    return _serviceEnabled && (_permissionGranted == PermissionStatus.granted);
+
+  }
+
+  void _getLocation() async{
+    if (! await _checkLocationServiceStatus()) return;
+    LocationData newLok = await _locationService.getLocation();
+
+    setState(() {
+      _locationData = new LocationPoint(lat: newLok.latitude!, long: newLok.longitude!, name: "name");
+    });
+  }
+
+  void _checkNearObjects() async{
+    if (! await _checkLocationServiceStatus()) return;
+
+  }
+
   void _addPoint() {
     setState(() {
       this._points.add(new LocationPoint(
@@ -25,6 +68,12 @@ class _Lab7State extends State<Lab7> {
           name: _nameController.text
       ));
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
   }
 
   @override
@@ -39,6 +88,7 @@ class _Lab7State extends State<Lab7> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text("lat: " + _locationData.lat.toString() + ", long: " + _locationData.long.toString()),
             TextField(
               controller: this._latController,
               decoration: InputDecoration(
